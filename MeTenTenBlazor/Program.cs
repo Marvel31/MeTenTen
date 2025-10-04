55ed2d4c61db32b0c8c7d7a8e2238a575ef3d180
jongm <jongm@DESKTOP-E09PPQ1>
2025-10-04 22:48:38 +0900

    Convert to Blazor WebAssembly for Vercel deployment

--- MeTenTenBlazor/Program.cs
+++ MeTenTenBlazor/Program.cs
@@ -1,33 +1,17 @@
-using MeTenTenBlazor.Components;
 using MeTenTenBlazor.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Add services for Vercel deployment (local storage based)
 builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
 builder.Services.AddScoped<ITopicService, LocalTopicService>();
 builder.Services.AddScoped<ITenTenService, LocalTenTenService>();
 
-var app = builder.Build();
+// Add HttpClient
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
